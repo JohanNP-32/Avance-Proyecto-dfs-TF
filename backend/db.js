47dcda6da8eefeb3a3c1d33e0bea4719db419ac9
JohanNP-32 <johan.nunez2002@gmail.com>
2026-02-02 21:50:52 -0600

    v4

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql2');
 const path = require('path');
 
-// 1. Forzamos la carga del archivo .env usando la ruta exacta de este archivo
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
-// 2. IMPRESIÓN DE DEPURACIÓN (Revisa tu terminal al guardar)
 console.log("--- DEBUG CONEXIÓN ---");
 console.log("Usuario leído:", process.env.DB_USER);
 console.log("Base de datos:", process.env.DB_NAME);
@@ -12,7 +10,7 @@ console.log("----------------------");
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER, // Si esto sigue fallando, escribe aquí 'root' directamente para probar
+    user: process.env.DB_USER, 
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
